5f4526a1cfc21064f8fb242463d5e4a7861d0ef4
Jason Stratman <jason@stratmanmedia.com>
2021-11-21 20:16:51 -0600

    Added Domain Contracts and Infrastructure projects

---
+++ src/Shopperior.Api/Shopperior.Logging.DependencyInjection.Microsoft/ServiceRegistrar.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Shopperior.Logging.DependencyInjection.Microsoft
+{
+    public class ServiceRegistrar
+    {
+        internal static void AddRequiredServices(IServiceCollection services, ShopperiorLoggerConfiguration configuration)
+        {
+            services.AddScoped<IShopperiorLogger>(provider => new ShopperiorLogger(configuration));
+        }
+    }
+}
---
+++ src/Shopperior.Api/StratmanMedia.Repositories.EFCore/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace StratmanMedia.Repositories.EFCore
+{
+    public class Repository<TContext, TEntity> : IRepository<TEntity>
+        where TContext : DbContext
+        where TEntity : class
+    {
+        protected TContext Context { get; }
+        protected DbSet<TEntity> Table => Context.Set<TEntity>();
+
+        public Repository(TContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public virtual void Create(TEntity entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+            Table.Add(entity);
+            Context.SaveChanges();
+        }
+
+        public virtual async Task CreateAsync(TEntity entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+            await Table.AddAsync(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+            Table.Attach(entity);
+            Table.Remove(entity);
+            Context.SaveChanges();
+        }
+
+        public virtual async Task DeleteAsync(TEntity entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+            Table.Attach(entity);
+            Table.Remove(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        public virtual IEnumerable<TEntity> GetAll()
+        {
+            return Table.ToArray();
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await Task.Run(GetAll);
+        }
+    }
+}
