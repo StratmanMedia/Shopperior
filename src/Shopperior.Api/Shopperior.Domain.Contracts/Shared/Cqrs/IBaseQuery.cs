4b292dc66dd53025d50ed072b5f59ad4fab5048a
Jason Stratman <jason@stratmanmedia.com>
2022-01-03 15:36:33 -0600

    Created get all shopping lists interface

---
+++ src/Shopperior.Api/Shopperior.Domain.Contracts/Shared/Cqrs/IBaseQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace Shopperior.Domain.Contracts.Shared.Cqrs;
+
+public interface IBaseQuery<TResponse>
+{
+    Task<TResponse> ExecuteAsync();
+}
+
+public interface IBaseQuery<TRequest, TResponse>
+{
+    Task<TResponse> ExecuteAsyc(TRequest request);
+}
---
+++ src/Shopperior.Api/Shopperior.Domain.Contracts/ShoppingLists/Queries/IGetAllShoppingListsQuery.cs
@@ -0,0 +1,11 @@
+﻿using Shopperior.Domain.Contracts.Shared.Cqrs;
+using Shopperior.Domain.Entities;
+
+namespace Shopperior.Domain.Contracts.ShoppingLists.Queries;
+
+public interface IGetAllShoppingListsQuery : 
+    IBaseQuery<IEnumerable<ShoppingList>>, 
+    IBaseQuery<long, IEnumerable<ShoppingList>>,
+    IBaseQuery<Guid, IEnumerable<ShoppingList>>
+{
+}
