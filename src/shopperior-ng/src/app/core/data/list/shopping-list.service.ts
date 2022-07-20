0b82d44b1fa49f620b4b6a98093b369b4ed95b3c
Jason Stratman <jason@stratmanmedia.com>
2022-07-19 20:32:47 -0500

    Loading shopping list data from storage on initial request

--- src/shopperior-ng/src/app/core/data/list/shopping-list.service.ts
+++ src/shopperior-ng/src/app/core/data/list/shopping-list.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, ReplaySubject } from 'rxjs';
-import { map, take, tap } from 'rxjs/operators';
+import { concatMap, map, take, tap } from 'rxjs/operators';
 import { constants } from 'src/app/shared/classes/constants';
 import { environment } from 'src/environments/environment';
 import { LoggingService } from '../../logging/logging.service';
@@ -22,6 +22,8 @@ export class ShoppingListService {
   constructor(
     private _local: LocalDataService,
     private _api: ShopperiorApiService) {
+      this._logger.debug('Loading shopping lists from storage.');
+      this._listSubject.next(this._local.get<ShoppingListModel[]>(constants.storageKeys.shoppingLists));
       this.loadLists();
       this._listSubject.subscribe(
         (lists: ShoppingListModel[]) => {
@@ -48,25 +50,25 @@ export class ShoppingListService {
   public add(shoppingList: ShoppingListModel): Observable<void> {
     return this._listSubject.pipe(
       take(1),
-      map(lists => {
-        this._api.ShoppingLists.add(shoppingList).subscribe(
-          guid => {
-            this.loadLists();
-          }
-        );
-      })
+      concatMap(lists => {
+        lists.push(shoppingList);
+        this._listSubject.next(lists);
+        return this._api.ShoppingLists.add(shoppingList).pipe(take(1));
+      }),
+      map(guid => this.loadLists())
     );
   }
 
   public delete(guid: string): Observable<void> {
     return this._listSubject.pipe(
       take(1),
-      map(lists => {
+      concatMap(lists => {
         const index = lists.findIndex(a => a.guid === guid);
         lists.splice(index, 1);
         this._listSubject.next(lists);
-        this._api.ShoppingLists.delete(guid).pipe(take(1)).subscribe();
-      })
+        return this._api.ShoppingLists.delete(guid).pipe(take(1));
+      }),
+      map(() => this.loadLists())
     );
   }
 
@@ -74,11 +76,13 @@ export class ShoppingListService {
     this._logger.debug('Updating shopping list: ' + JSON.stringify(shoppingList));
     return this._listSubject.pipe(
       take(1),
-      map(lists => {
-        this._api.ShoppingLists.update(shoppingList).pipe(take(1)).subscribe(() => {
-          this.loadLists();
-        });
-      })
+      concatMap(lists => {
+        let foundList = lists.find(l => l.guid === shoppingList.guid);
+        foundList = {...shoppingList};
+        this._listSubject.next(lists);
+        return this._api.ShoppingLists.update(shoppingList).pipe(take(1));
+      }),
+      map(() => this.loadLists())
     );
   }
 
@@ -86,13 +90,13 @@ export class ShoppingListService {
     this._logger.debug(`Adding item. ${JSON.stringify(item)}`);
     return this._listSubject.pipe(
       take(1),
-      map(lists => {
-        this._api.ShoppingLists.addItem(item).subscribe(
-          () => {
-            this.loadLists();
-          }
-        );
-      })
+      concatMap(lists => {
+        var foundList = lists.find(l => l.guid === item.shoppingListGuid);
+        foundList.items.push(item);
+        this._listSubject.next(lists);
+        return this._api.ShoppingLists.addItem(item).pipe(take(1));
+      }),
+      map(() => this.loadLists())
     );
   }
 
@@ -100,17 +104,14 @@ export class ShoppingListService {
     this._logger.debug(`Updating item. ${JSON.stringify(item)}`);
     return this._listSubject.pipe(
       take(1),
-      map(lists => {
+      concatMap(lists => {
         let foundList = lists.find(l => l.guid === item.shoppingListGuid);
         let foundItem = foundList.items.find(i => i.guid === item.guid);
         foundItem = {...item};
         this._listSubject.next(lists);
-        this._api.ShoppingLists.updateItem(item).pipe(take(1)).subscribe(
-          () => {
-            this.loadLists();
-          }
-        );
-      })
+        return this._api.ShoppingLists.updateItem(item).pipe(take(1));
+      }),
+      map(() => this.loadLists())
     );
   }
 
